a1710533efee61b6c3b80262c4bf79d614bcb8d0
Jacek Szarski <jacek@applicake.com>
2011-12-11 23:39:28 +0100

    created the first trace script

---
+++ trace.sh
@@ -0,0 +1,8 @@
+#COMMAND='tracepath'
+COMMAND='traceroute'
+OUTPUT_DIR='output'
+mkdir -p $OUTPUT_DIR
+RESOURCE='wp.pl'
+TIMESTAMP=`date +%s`
+FILENAME=`echo $OUTPUT_DIR'/'$TIMESTAMP'_'$RESOURCE`
+$COMMAND $RESOURCE > $FILENAME
