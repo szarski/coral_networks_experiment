19f072da89f67546c962f6ac1a9031c461169aea
Jacek Szarski <jacek@applicake.com>
2011-12-12 00:10:37 +0100

    resources config fix

--- trace.sh
+++ trace.sh
@@ -3,8 +3,9 @@ COMMAND='traceroute'
 OUTPUT_DIR='output'
 mkdir -p $OUTPUT_DIR
 TIMESTAMP=`date +%s`
+RESOURCES=`cat config/resources.txt`
 
-for RESOURCE in `cat config/resources.txt`; do
+for RESOURCE in $RESOURCES; do
   FILENAME=`echo $OUTPUT_DIR'/'$TIMESTAMP'_'$RESOURCE`
   $COMMAND $RESOURCE > $FILENAME
 done
