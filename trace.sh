c0c7095f0ebe8cc43a75ddcf8fadf63b5b508387
Jacek Szarski <jacek@applicake.com>
2011-12-13 11:59:14 +0100

    dump command fix

--- trace.sh
+++ trace.sh
@@ -1,6 +1,6 @@
 COMMAND=`(hash traceroute 2>&- && echo 'traceroute') || echo 'tracepath'`
 DNS_COMMAND='dig'
-DUMP_COMMAND='sudo tcpdump'
+DUMP_COMMAND='sudo tcpdump -i eth0 -w'
 OUTPUT_DIR='output'
 TIMESTAMP=`date +%s`
 RESOURCES=`cat config/resources.txt`
@@ -11,7 +11,7 @@ mkdir -p $OUTPUT_DIR'/dns'
 
 for RESOURCE in $RESOURCES; do
   DUMP_FILENAME=`echo $OUTPUT_DIR'/dumps/'$TIMESTAMP'_'$RESOURCE`
-  $DUMP_COMMAND > $DUMP_FILENAME &
+  $DUMP_COMMAND $DUMP_FILENAME &
 
   FILENAME=`echo $OUTPUT_DIR'/traces/'$TIMESTAMP'_'$RESOURCE`
   $COMMAND $RESOURCE > $FILENAME
