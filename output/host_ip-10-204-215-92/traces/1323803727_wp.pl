20db810a2753d0f5a6dca12e5afe3f254c9ac740
ip-10-204-215-92 <ubuntu@ip-10-204-215-92.ec2.internal>
2011-12-13 19:15:30 +0000

    test results: 1323803727

---
+++ output/host_ip-10-204-215-92/traces/1323803727_wp.pl
@@ -0,0 +1,17 @@
+ 1:  ip-10-204-215-92.ec2.internal                         0.113ms pmtu 1500
+ 1:  ip-10-204-212-2.ec2.internal                          1.179ms asymm  2 
+ 1:  ip-10-204-212-2.ec2.internal                          4.419ms asymm  2 
+ 2:  ip-10-1-36-25.ec2.internal                            0.539ms 
+ 3:  ip-10-1-43-14.ec2.internal                            0.523ms 
+ 4:  216.182.232.52                                        0.854ms 
+ 5:  72.21.220.241                                         0.709ms asymm  4 
+ 6:  72.21.222.154                                         1.753ms asymm  5 
+ 7:  72.21.220.54                                          2.094ms asymm  8 
+ 8:  ash-bb1-link.telia.net                                1.964ms asymm 10 
+ 9:  hbg-bb1-link.telia.net                              135.456ms asymm 12 
+10:  war-b2-link.telia.net                               111.143ms asymm 12 
+11:  atm-ic-144386-war-b2.c.telia.net                    122.466ms asymm 18 
+12:  do-atman-global.wp.pl                               117.167ms asymm 17 
+13:  rtr4.rtr-int-1.adm.wp-sa.pl                         115.716ms asymm 19 
+14:  www.wp.pl                                           116.994ms reached
+     Resume: pmtu 1500 hops 14 back 236 
