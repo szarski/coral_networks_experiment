7378cc7099ccac68e9cab315be310117d8c30db1
domU-12-31-39-0A-94-2A <ubuntu@domU-12-31-39-0A-94-2A.compute-1.internal>
2011-12-13 23:00:07 +0000

    test results: 1323817201

---
+++ output/host_domU-12-31-39-0A-94-2A/traces/1323817201_wp.pl
@@ -0,0 +1,17 @@
+ 1:  domU-12-31-39-0A-94-2A.compute-1.internal             0.126ms pmtu 1500
+ 1:  10.211.148.2                                          0.534ms asymm  2 
+ 1:  10.211.148.2                                          0.773ms asymm  2 
+ 2:  ip-10-1-8-17.ec2.internal                             0.539ms 
+ 3:  ip-10-1-9-14.ec2.internal                             0.741ms 
+ 4:  216.182.224.76                                        0.654ms 
+ 5:  72.21.220.225                                         0.699ms asymm  4 
+ 6:  72.21.222.148                                         0.873ms asymm  5 
+ 7:  72.21.220.28                                          1.105ms 
+ 8:  ash-bb1-link.telia.net                                1.455ms asymm 11 
+ 9:  hbg-bb1-link.telia.net                               94.314ms asymm 10 
+10:  war-b2-link.telia.net                               111.066ms asymm 12 
+11:  atm-ic-144386-war-b2.c.telia.net                    201.701ms asymm 15 
+12:  do-atman-global.wp.pl                               265.830ms asymm 16 
+13:  rtr4.rtr-int-1.adm.wp-sa.pl                         115.830ms asymm 16 
+14:  www.wp.pl                                           120.481ms reached
+     Resume: pmtu 1500 hops 14 back 239 
