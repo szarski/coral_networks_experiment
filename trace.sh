fa5c5232fd7ae7c42f6a51a08b04edc2a080bb74
Jacek Szarski <jacek@applicake.com>
2011-12-13 11:33:20 +0100

    updated the trace script to run more experiments

--- trace.sh
+++ trace.sh
@@ -1,10 +1,23 @@
 COMMAND=`(hash traceroute 2>&- && echo 'traceroute') || echo 'tracepath'`
+DNS_COMMAND='dig'
+DUMP_COMMAND='sudo tcpdump'
 OUTPUT_DIR='output'
-mkdir -p $OUTPUT_DIR
 TIMESTAMP=`date +%s`
 RESOURCES=`cat config/resources.txt`
 
+mkdir -p $OUTPUT_DIR'/dumps'
+mkdir -p $OUTPUT_DIR'/traces'
+mkdir -p $OUTPUT_DIR'/dns'
+
 for RESOURCE in $RESOURCES; do
-  FILENAME=`echo $OUTPUT_DIR'/'$TIMESTAMP'_'$RESOURCE`
+  DUMP_FILENAME=`echo $OUTPUT_DIR'/dumps/'$TIMESTAMP'_'$RESOURCE`
+  $DUMP_COMMAND > $DUMP_FILENAME &
+
+  FILENAME=`echo $OUTPUT_DIR'/traces/'$TIMESTAMP'_'$RESOURCE`
   $COMMAND $RESOURCE > $FILENAME
+
+  DNS_FILENAME=`echo $OUTPUT_DIR'/dns/'$TIMESTAMP'_'$RESOURCE`
+  $DNS_COMMAND $RESOURCE > $DNS_FILENAME
+
+  killall $DUMP_COMMAND
 done
