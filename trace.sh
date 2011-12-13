8fe3fc48dddff90ff1234060cf86bb10f3e9500a
Jacek Szarski <jacek@applicake.com>
2011-12-13 19:41:18 +0100

    made trace script commit to git

--- trace.sh
+++ trace.sh
@@ -1,8 +1,9 @@
+BASE_DIR='~/coral_networks_experiment'
 COMMAND=`(hash traceroute 2>&- && echo 'traceroute') || echo 'tracepath'`
 DNS_COMMAND='dig'
 DUMP_COMMAND='sudo tcpdump -i eth0 -w'
 DATA_COMMAND='wget'
-OUTPUT_DIR='output/host_'`hostname`
+OUTPUT_DIR=$BASE_DIR'/output/host_'`hostname`
 TIMESTAMP=`date +%s`
 RESOURCES=`cat config/resources.txt`
 
@@ -12,6 +13,8 @@ mkdir -p $OUTPUT_DIR'/dns'
 mkdir -p $OUTPUT_DIR'/data'
 
 for RESOURCE in $RESOURCES; do
+  cd $BASE_DIR && git pull
+
   DUMP_FILENAME=`echo $OUTPUT_DIR'/dumps/'$TIMESTAMP'_'$RESOURCE`
   $DUMP_COMMAND $DUMP_FILENAME &
 
@@ -25,4 +28,6 @@ for RESOURCE in $RESOURCES; do
   $DATA_COMMAND $RESOURCE $DATA_FILENAME
 
   sudo killall tcpdump
+
+  cd $BASE_DIR && git add $OUTPUT_DIR && git commit -m'test results: '$TIMESTAMP
 done
