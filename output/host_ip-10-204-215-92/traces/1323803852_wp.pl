e997fec1fbb472008fd6b2d1b9ed76e449dabbd5
ip-10-204-215-92 <ubuntu@ip-10-204-215-92.ec2.internal>
2011-12-13 19:17:34 +0000

    test results: 1323803852

---
+++ output/host_ip-10-204-215-92/traces/1323803852_wp.pl
@@ -0,0 +1,16 @@
+ 1:  ip-10-204-215-92.ec2.internal                         0.104ms pmtu 1500
+ 1:  ip-10-204-212-3.ec2.internal                          0.427ms asymm  2 
+ 1:  ip-10-204-212-3.ec2.internal                          0.868ms asymm  2 
+ 2:  ip-10-1-50-25.ec2.internal                            0.473ms 
+ 3:  ip-10-1-45-14.ec2.internal                            0.685ms 
+ 4:  216.182.224.92                                        0.565ms 
+ 5:  72.21.222.154                                         2.012ms 
+ 6:  72.21.220.62                                          1.956ms asymm  8 
+ 7:  ash-bb1-link.telia.net                               20.650ms asymm 10 
+ 8:  hbg-bb1-link.telia.net                               94.954ms asymm 12 
+ 9:  war-b2-link.telia.net                               110.599ms asymm 10 
+10:  atm-ic-144386-war-b2.c.telia.net                    119.612ms asymm 18 
+11:  do-atman-global.wp.pl                               124.445ms asymm 17 
+12:  rtr4.rtr-int-1.adm.wp-sa.pl                         121.321ms asymm 19 
+13:  www.wp.pl                                           117.220ms reached
+     Resume: pmtu 1500 hops 13 back 236 
