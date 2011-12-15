96c48736d71306c14dbb531217918a5415918165
ip-10-244-177-91 <ubuntu@ip-10-244-177-91.ec2.internal>
2011-12-15 01:00:04 +0000

    test results: 1323910801

---
+++ output/host_ip-10-244-177-91/traces/1323910801_wp.pl
@@ -0,0 +1,16 @@
+ 1:  ip-10-244-177-91.ec2.internal                         0.136ms pmtu 1500
+ 1:  ip-10-244-176-3.ec2.internal                          0.716ms asymm  2 
+ 1:  ip-10-244-176-3.ec2.internal                          0.741ms asymm  2 
+ 2:  ip-10-1-46-1.ec2.internal                             0.598ms 
+ 3:  ip-10-1-47-14.ec2.internal                            0.539ms 
+ 4:  216.182.232.54                                        0.766ms 
+ 5:  72.21.222.154                                         1.708ms 
+ 6:  72.21.220.30                                          1.550ms 
+ 7:  ash-bb1-link.telia.net                                1.959ms asymm  8 
+ 8:  hbg-bb1-link.telia.net                              137.095ms asymm 12 
+ 9:  war-b2-link.telia.net                               110.959ms asymm 11 
+10:  atm-ic-144386-war-b2.c.telia.net                    124.127ms asymm 18 
+11:  do-atman-global.wp.pl                               115.256ms asymm 15 
+12:  rtr4.rtr-int-1.adm.wp-sa.pl                         121.379ms asymm 18 
+13:  www.wp.pl                                           120.908ms reached
+     Resume: pmtu 1500 hops 13 back 236 
