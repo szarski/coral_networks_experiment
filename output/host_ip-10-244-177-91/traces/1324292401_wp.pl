933a146f5a6baf5f06a22dff866cd6f8f03140bc
ip-10-244-177-91 <ubuntu@ip-10-244-177-91.ec2.internal>
2011-12-19 11:00:05 +0000

    test results: 1324292401

---
+++ output/host_ip-10-244-177-91/traces/1324292401_wp.pl
@@ -0,0 +1,17 @@
+ 1:  ip-10-244-177-91.ec2.internal                         0.106ms pmtu 1500
+ 1:  ip-10-244-176-2.ec2.internal                          0.638ms asymm  2 
+ 1:  ip-10-244-176-2.ec2.internal                          0.578ms asymm  2 
+ 2:  ip-10-1-36-1.ec2.internal                             0.484ms 
+ 3:  ip-10-1-37-14.ec2.internal                            0.525ms 
+ 4:  216.182.224.86                                        0.583ms 
+ 5:  72.21.220.241                                         0.710ms asymm  4 
+ 6:  72.21.222.154                                         1.891ms asymm  5 
+ 7:  72.21.220.54                                          2.045ms asymm  9 
+ 8:  ash-bb1-link.telia.net                               40.233ms 
+ 9:  hbg-bb2-link.telia.net                               94.893ms asymm 12 
+10:  war-b2-link.telia.net                               110.524ms 
+11:  atm-ic-144386-war-b2.c.telia.net                    122.484ms asymm 18 
+12:  do-atman-global.wp.pl                               115.454ms asymm 15 
+13:  rtr4.rtr-int-1.adm.wp-sa.pl                         122.395ms asymm 18 
+14:  www.wp.pl                                           116.238ms reached
+     Resume: pmtu 1500 hops 14 back 236 
