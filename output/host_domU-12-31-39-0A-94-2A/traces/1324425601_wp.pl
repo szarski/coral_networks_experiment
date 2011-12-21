5bc025506737e85b420d4f13421647a73fa64ef5
domU-12-31-39-0A-94-2A <ubuntu@domU-12-31-39-0A-94-2A.compute-1.internal>
2011-12-21 00:00:05 +0000

    test results: 1324425601

---
+++ output/host_domU-12-31-39-0A-94-2A/traces/1324425601_wp.pl
@@ -0,0 +1,17 @@
+ 1:  domU-12-31-39-0A-94-2A.compute-1.internal             0.124ms pmtu 1500
+ 1:  10.211.148.2                                          2.608ms asymm  2 
+ 1:  10.211.148.2                                          0.582ms asymm  2 
+ 2:  ip-10-1-8-17.ec2.internal                             0.470ms 
+ 3:  ip-10-1-9-14.ec2.internal                             0.504ms 
+ 4:  216.182.224.76                                        0.624ms 
+ 5:  72.21.220.225                                         0.689ms asymm  4 
+ 6:  72.21.222.148                                         0.982ms asymm  5 
+ 7:  72.21.220.60                                         25.270ms asymm  8 
+ 8:  ash-bb1-link.telia.net                               39.330ms asymm 11 
+ 9:  hbg-bb2-link.telia.net                               94.300ms asymm 11 
+10:  war-b2-link.telia.net                               110.990ms asymm 13 
+11:  atm-ic-144386-war-b2.c.telia.net                    115.812ms asymm 15 
+12:  do-atman-global.wp.pl                               120.609ms asymm 16 
+13:  rtr4.rtr-int-1.adm.wp-sa.pl                         130.675ms asymm 16 
+14:  www.wp.pl                                           115.892ms reached
+     Resume: pmtu 1500 hops 14 back 239 
