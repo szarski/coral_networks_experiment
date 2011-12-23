a365fb65ac81ebd54895003a0597fc3efbb37c6e
domU-12-31-39-0A-94-2A <ubuntu@domU-12-31-39-0A-94-2A.compute-1.internal>
2011-12-23 08:00:05 +0000

    test results: 1324627201

---
+++ output/host_domU-12-31-39-0A-94-2A/traces/1324627201_wp.pl
@@ -0,0 +1,17 @@
+ 1:  domU-12-31-39-0A-94-2A.compute-1.internal             0.109ms pmtu 1500
+ 1:  10.211.148.2                                          0.884ms asymm  2 
+ 1:  10.211.148.2                                          0.448ms asymm  2 
+ 2:  ip-10-1-8-17.ec2.internal                             0.564ms 
+ 3:  ip-10-1-9-14.ec2.internal                             0.912ms 
+ 4:  216.182.224.72                                        0.783ms 
+ 5:  72.21.220.225                                         0.775ms asymm  4 
+ 6:  72.21.222.148                                         1.080ms asymm  5 
+ 7:  72.21.220.52                                          1.293ms asymm  8 
+ 8:  ash-bb1-link.telia.net                                1.336ms asymm 10 
+ 9:  hbg-bb2-link.telia.net                               94.295ms asymm 11 
+10:  war-b2-link.telia.net                               110.057ms 
+11:  atm-ic-144386-war-b2.c.telia.net                    116.506ms asymm 15 
+12:  do-atman-global.wp.pl                               116.040ms asymm 16 
+13:  rtr4.rtr-int-1.adm.wp-sa.pl                         115.700ms asymm 16 
+14:  www.wp.pl                                           123.202ms reached
+     Resume: pmtu 1500 hops 14 back 239 
