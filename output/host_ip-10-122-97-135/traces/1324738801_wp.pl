37ab31507f134e3933c610455567e986cc071def
ip-10-122-97-135 <ubuntu@ip-10-122-97-135.ec2.internal>
2011-12-24 15:00:05 +0000

    test results: 1324738801

---
+++ output/host_ip-10-122-97-135/traces/1324738801_wp.pl
@@ -0,0 +1,17 @@
+ 1:  ip-10-122-97-135.ec2.internal                         0.116ms pmtu 1500
+ 1:  ip-10-122-96-2.ec2.internal                           0.506ms asymm  2 
+ 1:  ip-10-122-96-2.ec2.internal                           0.981ms asymm  2 
+ 2:  ip-10-1-106-17.ec2.internal                           3.223ms 
+ 3:  ip-10-1-107-6.ec2.internal                            0.577ms 
+ 4:  216.182.224.62                                        0.578ms 
+ 5:  72.21.220.193                                         0.669ms asymm  4 
+ 6:  72.21.222.140                                         1.268ms asymm  5 
+ 7:  72.21.220.64                                          1.275ms asymm  9 
+ 8:  ash-bb1-link.telia.net                               79.880ms asymm 11 
+ 9:  hbg-bb1-link.telia.net                               94.513ms asymm 10 
+10:  war-b2-link.telia.net                               110.261ms 
+11:  atm-ic-144386-war-b2.c.telia.net                    137.171ms asymm 18 
+12:  do-atman-global.wp.pl                               126.132ms asymm 17 
+13:  rtr4.rtr-int-2.adm.wp-sa.pl                         115.294ms asymm 19 
+14:  www.wp.pl                                           115.028ms reached
+     Resume: pmtu 1500 hops 14 back 237 
