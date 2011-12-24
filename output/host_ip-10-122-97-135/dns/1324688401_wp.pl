70166acda80558a807b1c2166342733542e29ad3
ip-10-122-97-135 <ubuntu@ip-10-122-97-135.ec2.internal>
2011-12-24 01:00:05 +0000

    test results: 1324688401

---
+++ output/host_ip-10-122-97-135/dns/1324688401_wp.pl
@@ -0,0 +1,18 @@
+
+; <<>> DiG 9.7.3 <<>> wp.pl
+;; global options: +cmd
+;; Got answer:
+;; ->>HEADER<<- opcode: QUERY, status: NOERROR, id: 32165
+;; flags: qr rd ra; QUERY: 1, ANSWER: 1, AUTHORITY: 0, ADDITIONAL: 0
+
+;; QUESTION SECTION:
+;wp.pl.				IN	A
+
+;; ANSWER SECTION:
+wp.pl.			3600	IN	A	212.77.100.101
+
+;; Query time: 132 msec
+;; SERVER: 172.16.0.23#53(172.16.0.23)
+;; WHEN: Sat Dec 24 01:00:03 2011
+;; MSG SIZE  rcvd: 39
+
---
+++ output/host_ip-10-122-97-135/traces/1324688401_wp.pl
@@ -0,0 +1,16 @@
+ 1:  ip-10-122-97-135.ec2.internal                         0.106ms pmtu 1500
+ 1:  ip-10-122-96-3.ec2.internal                           0.725ms asymm  2 
+ 1:  ip-10-122-96-3.ec2.internal                           0.862ms asymm  2 
+ 2:  ip-10-1-122-17.ec2.internal                           0.612ms 
+ 3:  ip-10-1-123-6.ec2.internal                            0.651ms 
+ 4:  216.182.224.50                                        0.659ms 
+ 5:  72.21.222.140                                         1.345ms 
+ 6:  72.21.220.24                                          1.277ms 
+ 7:  ash-bb1-link.telia.net                                1.551ms asymm 11 
+ 8:  hbg-bb1-link.telia.net                               94.526ms asymm 10 
+ 9:  war-b2-link.telia.net                               110.193ms asymm 10 
+10:  atm-ic-144386-war-b2.c.telia.net                    110.361ms asymm 18 
+11:  do-atman-global.wp.pl                               127.120ms asymm 17 
+12:  rtr4.rtr-int-2.adm.wp-sa.pl                         121.272ms asymm 19 
+13:  www.wp.pl                                           123.256ms reached
+     Resume: pmtu 1500 hops 13 back 237 
