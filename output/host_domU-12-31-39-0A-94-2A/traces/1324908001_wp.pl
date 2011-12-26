2a2729ff3003a2c87807138f5d055883730ca68b
domU-12-31-39-0A-94-2A <ubuntu@domU-12-31-39-0A-94-2A.compute-1.internal>
2011-12-26 14:00:06 +0000

    test results: 1324908001

---
+++ output/host_domU-12-31-39-0A-94-2A/traces/1324908001_wp.pl
@@ -0,0 +1,16 @@
+ 1:  domU-12-31-39-0A-94-2A.compute-1.internal             0.118ms pmtu 1500
+ 1:  10.211.148.3                                        110.477ms asymm  2 
+ 1:  10.211.148.3                                          1.254ms asymm  2 
+ 2:  ip-10-1-18-17.ec2.internal                            0.476ms 
+ 3:  ip-10-1-19-14.ec2.internal                            0.523ms 
+ 4:  216.182.224.78                                        0.883ms 
+ 5:  72.21.222.148                                         1.052ms 
+ 6:  72.21.220.52                                         13.164ms asymm  8 
+ 7:  ash-bb1-link.telia.net                                1.375ms asymm 10 
+ 8:  hbg-bb2-link.telia.net                               94.223ms asymm 11 
+ 9:  war-b2-link.telia.net                               121.680ms asymm 13 
+10:  atm-ic-144386-war-b2.c.telia.net                    111.048ms asymm 15 
+11:  do-atman-global.wp.pl                               115.874ms asymm 16 
+12:  rtr4.rtr-int-1.adm.wp-sa.pl                         121.511ms asymm 16 
+13:  www.wp.pl                                           114.618ms reached
+     Resume: pmtu 1500 hops 13 back 239 
