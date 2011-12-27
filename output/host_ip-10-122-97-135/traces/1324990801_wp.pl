3bb770807c199bd8231be3dad24f6681f4c0170d
ip-10-122-97-135 <ubuntu@ip-10-122-97-135.ec2.internal>
2011-12-27 13:00:05 +0000

    test results: 1324990801

---
+++ output/host_ip-10-122-97-135/traces/1324990801_wp.pl
@@ -0,0 +1,17 @@
+ 1:  ip-10-122-97-135.ec2.internal                         0.107ms pmtu 1500
+ 1:  ip-10-122-96-2.ec2.internal                           0.517ms asymm  2 
+ 1:  ip-10-122-96-2.ec2.internal                           0.987ms asymm  2 
+ 2:  ip-10-1-106-17.ec2.internal                           0.509ms 
+ 3:  ip-10-1-107-6.ec2.internal                            0.516ms 
+ 4:  216.182.224.62                                        0.871ms 
+ 5:  72.21.220.193                                         0.769ms asymm  4 
+ 6:  72.21.222.140                                         1.459ms asymm  5 
+ 7:  72.21.220.48                                          1.786ms asymm  9 
+ 8:  ash-bb1-link.telia.net                                1.422ms asymm 11 
+ 9:  hbg-bb1-link.telia.net                               95.282ms asymm 10 
+10:  war-b2-link.telia.net                               110.556ms asymm 12 
+11:  atm-ic-144386-war-b2.c.telia.net                    121.053ms asymm 18 
+12:  do-atman-global.wp.pl                               123.696ms asymm 17 
+13:  rtr4.rtr-int-2.adm.wp-sa.pl                         116.383ms asymm 19 
+14:  www.wp.pl                                           115.223ms reached
+     Resume: pmtu 1500 hops 14 back 237 
