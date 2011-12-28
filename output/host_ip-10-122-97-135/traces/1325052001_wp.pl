bd92632ae00be47ba713caddffd978ca37a0d479
ip-10-122-97-135 <ubuntu@ip-10-122-97-135.ec2.internal>
2011-12-28 06:00:04 +0000

    test results: 1325052001

---
+++ output/host_ip-10-122-97-135/traces/1325052001_wp.pl
@@ -0,0 +1,17 @@
+ 1:  ip-10-122-97-135.ec2.internal                         0.120ms pmtu 1500
+ 1:  ip-10-122-96-2.ec2.internal                           0.531ms asymm  2 
+ 1:  ip-10-122-96-2.ec2.internal                           0.729ms asymm  2 
+ 2:  ip-10-1-102-17.ec2.internal                           0.639ms 
+ 3:  ip-10-1-103-6.ec2.internal                            0.483ms 
+ 4:  216.182.224.48                                        0.661ms 
+ 5:  72.21.220.193                                         0.551ms asymm  4 
+ 6:  72.21.222.140                                         1.390ms asymm  5 
+ 7:  72.21.220.40                                          1.454ms asymm  9 
+ 8:  ash-bb1-link.telia.net                                1.167ms 
+ 9:  hbg-bb1-link.telia.net                               94.541ms asymm 10 
+10:  war-b2-link.telia.net                               110.347ms asymm 13 
+11:  atm-ic-144386-war-b2.c.telia.net                    119.212ms asymm 18 
+12:  do-atman-global.wp.pl                               123.722ms asymm 17 
+13:  rtr4.rtr-int-2.adm.wp-sa.pl                         115.415ms asymm 19 
+14:  www.wp.pl                                           123.643ms reached
+     Resume: pmtu 1500 hops 14 back 237 
