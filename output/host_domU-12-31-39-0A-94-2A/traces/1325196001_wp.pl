2b2bf1824a1249ec5fe6fc20bddd13b1da955d76
domU-12-31-39-0A-94-2A <ubuntu@domU-12-31-39-0A-94-2A.compute-1.internal>
2011-12-29 22:00:05 +0000

    test results: 1325196001

---
+++ output/host_domU-12-31-39-0A-94-2A/traces/1325196001_wp.pl
@@ -0,0 +1,17 @@
+ 1:  domU-12-31-39-0A-94-2A.compute-1.internal             0.112ms pmtu 1500
+ 1:  10.211.148.2                                          0.723ms asymm  2 
+ 1:  10.211.148.2                                          0.573ms asymm  2 
+ 2:  ip-10-1-8-17.ec2.internal                             0.728ms 
+ 3:  ip-10-1-9-14.ec2.internal                             0.707ms 
+ 4:  216.182.224.76                                        0.673ms 
+ 5:  72.21.220.225                                         0.828ms asymm  4 
+ 6:  72.21.222.148                                        18.054ms asymm  5 
+ 7:  72.21.220.52                                          1.346ms asymm  8 
+ 8:  ash-bb1-link.telia.net                                1.307ms asymm 10 
+ 9:  hbg-bb2-link.telia.net                               94.801ms asymm 12 
+10:  war-b2-link.telia.net                               149.380ms 
+11:  atm-ic-144386-war-b2.c.telia.net                    111.095ms asymm 15 
+12:  do-atman-global.wp.pl                               115.938ms asymm 16 
+13:  rtr4.rtr-int-1.adm.wp-sa.pl                         133.510ms asymm 16 
+14:  www.wp.pl                                           120.119ms reached
+     Resume: pmtu 1500 hops 14 back 239 
