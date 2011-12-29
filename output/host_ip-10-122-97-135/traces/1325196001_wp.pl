5ecc2114e9556da05bcf8f28ec8ed28dead63588
ip-10-122-97-135 <ubuntu@ip-10-122-97-135.ec2.internal>
2011-12-29 22:00:05 +0000

    test results: 1325196001

---
+++ output/host_ip-10-122-97-135/traces/1325196001_wp.pl
@@ -0,0 +1,16 @@
+ 1:  ip-10-122-97-135.ec2.internal                         0.103ms pmtu 1500
+ 1:  ip-10-122-96-3.ec2.internal                           0.523ms asymm  2 
+ 1:  ip-10-122-96-3.ec2.internal                           0.978ms asymm  2 
+ 2:  ip-10-1-122-17.ec2.internal                           0.427ms 
+ 3:  ip-10-1-125-6.ec2.internal                            0.834ms 
+ 4:  216.182.224.68                                        0.876ms 
+ 5:  72.21.222.140                                         1.227ms 
+ 6:  72.21.220.32                                          1.462ms asymm  8 
+ 7:  ash-bb1-link.telia.net                               15.814ms asymm 11 
+ 8:  hbg-bb1-link.telia.net                               94.546ms asymm 10 
+ 9:  war-b2-link.telia.net                               110.401ms asymm 12 
+10:  atm-ic-144386-war-b2.c.telia.net                    119.053ms asymm 18 
+11:  do-atman-global.wp.pl                               116.502ms asymm 17 
+12:  rtr4.rtr-int-2.adm.wp-sa.pl                         126.816ms asymm 19 
+13:  www.wp.pl                                           126.166ms reached
+     Resume: pmtu 1500 hops 13 back 237 
