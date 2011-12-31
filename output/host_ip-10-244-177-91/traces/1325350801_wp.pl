46bb40b39ea5661f5d545b2314707d107b34fd24
ip-10-244-177-91 <ubuntu@ip-10-244-177-91.ec2.internal>
2011-12-31 17:00:05 +0000

    test results: 1325350801

---
+++ output/host_ip-10-244-177-91/traces/1325350801_wp.pl
@@ -0,0 +1,17 @@
+ 1:  ip-10-244-177-91.ec2.internal                         0.129ms pmtu 1500
+ 1:  ip-10-244-176-2.ec2.internal                          0.563ms asymm  2 
+ 1:  ip-10-244-176-2.ec2.internal                          0.685ms asymm  2 
+ 2:  ip-10-1-36-1.ec2.internal                             0.689ms 
+ 3:  ip-10-1-37-14.ec2.internal                            0.767ms 
+ 4:  216.182.224.86                                        0.701ms 
+ 5:  72.21.220.241                                         0.847ms asymm  4 
+ 6:  72.21.222.154                                         1.869ms asymm  5 
+ 7:  72.21.220.30                                          1.928ms asymm  6 
+ 8:  ash-bb1-link.telia.net                                2.040ms asymm 11 
+ 9:  hbg-bb1-link.telia.net                               95.275ms asymm 11 
+10:  war-b2-link.telia.net                               110.856ms asymm 13 
+11:  atm-ic-144386-war-b2.c.telia.net                    117.658ms asymm 18 
+12:  do-atman-global.wp.pl                               126.500ms asymm 15 
+13:  rtr4.rtr-int-1.adm.wp-sa.pl                         121.709ms asymm 18 
+14:  www.wp.pl                                           122.112ms reached
+     Resume: pmtu 1500 hops 14 back 236 
