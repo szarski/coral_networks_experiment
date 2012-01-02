58444db05656f15846fa41fccdb9d41de8744c0f
ip-10-244-177-91 <ubuntu@ip-10-244-177-91.ec2.internal>
2012-01-02 01:00:05 +0000

    test results: 1325466001

---
+++ output/host_ip-10-244-177-91/traces/1325466001_wp.pl
@@ -0,0 +1,16 @@
+ 1:  ip-10-244-177-91.ec2.internal                         0.127ms pmtu 1500
+ 1:  ip-10-244-176-3.ec2.internal                          0.693ms asymm  2 
+ 1:  ip-10-244-176-3.ec2.internal                          0.651ms asymm  2 
+ 2:  ip-10-1-46-1.ec2.internal                             0.725ms 
+ 3:  ip-10-1-47-14.ec2.internal                            1.069ms 
+ 4:  216.182.232.54                                        0.639ms 
+ 5:  72.21.222.154                                         1.701ms 
+ 6:  72.21.220.46                                          1.593ms 
+ 7:  ash-bb1-link.telia.net                                1.838ms asymm  8 
+ 8:  hbg-bb1-link.telia.net                               95.440ms asymm 11 
+ 9:  war-b2-link.telia.net                               111.194ms asymm 13 
+10:  atm-ic-144386-war-b2.c.telia.net                    112.013ms asymm 18 
+11:  do-atman-global.wp.pl                               115.440ms asymm 15 
+12:  rtr4.rtr-int-1.adm.wp-sa.pl                         117.552ms asymm 18 
+13:  www.wp.pl                                           123.667ms reached
+     Resume: pmtu 1500 hops 13 back 236 
